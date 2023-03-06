7774d848f4a098e4cd61fabd7d42bb549c34ce00
Paulius <pauliusdragunas@PAULIUSs-MacBook-Pro.local>
2023-03-06 10:30:27 -0800

    host permissions work

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,2 +1,16 @@
 console.log('This is the background page.');
 console.log('Put the background scripts here.');
+
+try {
+    chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+        if (changeInfo.status == 'complete') {
+            console.log('execute script about to run');
+            chrome.scripting.executeScript({
+                files: ['contentScript.bundle.js'],
+                target: { tabId: tab.id }
+            });
+        }
+    });
+} catch (e) {
+    console.log(e);
+}
