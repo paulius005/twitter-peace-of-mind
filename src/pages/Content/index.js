7da83d1b8d5c5e289642c5028df5189ee1e196f0
Paulius <pauliusdragunas@PAULIUSs-MacBook-Pro.local>
2023-03-06 15:52:41 -0800

    cleanup

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -13,16 +13,12 @@ setTimeout(() => {
 
 function bringPeaceOfMind() {
   const greatElonTweet = 'I am working on SpaceX & Tesla and it\'s going great!';
-  const articleArr = document.querySelectorAll('article');
   const elonTweets = $('article').find('span:contains("@elonmusk")').toArray();
 
   if (elonTweets.length) {
 
     elonTweets.forEach(elonTweetEl => {
       try {
-        // if this were a jquery object but that's not how jquery works
-        // const tweetSpan = elonTweetEl.closest('article').children(':first').children(':first').children().eq(1).children().eq(1).children().eq(1).children(':first').children(':first');
-
         const textSpanParent = elonTweetEl.closest('article').firstChild.firstChild.children[1].children[1].children[1].firstChild;
 
 
@@ -30,8 +26,6 @@ function bringPeaceOfMind() {
         if (!textSpanParent) {
           const textSpanParentParentParent = elonTweetEl.closest('article').firstChild.firstChild.children[1].children[1];
 
-          // for remove just the image
-          // textSpanParentParentParent.children[2].firstChild.firstChild.firstChild.firstChild.firstChild.remove();
           elonTweetEl.closest('article').remove();
 
           return;
@@ -52,8 +46,6 @@ function bringPeaceOfMind() {
           textSpanText.includes('liftoff') ||
           textSpanText.includes('energy')
         )) {
-          // if this were a jquery object but that's not how jquery works
-          // tweetSpan.text('I am working on SpaceX & Tesla and it\'s going great!')
           elonTweetEl.closest('article').firstChild.firstChild.children[1].children[1].children[1].firstChild.firstChild.textContent = 'I am working on SpaceX & Tesla and it\'s going great!';
           elonTweetEl.closest('article').firstChild.firstChild.children[1].children[1].children[2].firstChild.remove();
         }
