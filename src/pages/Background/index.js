0e26b4dac825feffb853016b7f5764d06033eae4
Paulius <pauliusdragunas@PAULIUSs-MacBook-Pro.local>
2023-03-06 14:32:30 -0800

    working for most stuff

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,16 +1,24 @@
 console.log('This is the background page.');
 console.log('Put the background scripts here.');
 
+console.log('paulius before try');
+
 try {
+    console.log('paulius try');
     chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
         if (changeInfo.status == 'complete') {
-            console.log('execute script about to run');
-            chrome.scripting.executeScript({
-                files: ['contentScript.bundle.js'],
-                target: { tabId: tab.id }
-            });
+            console.log('paulius complete');
+            // console.log('execute script about to run');
+            // chrome.scripting.executeScript({
+            //     files: ['contentScript.bundle.js'],
+            //     target: { tabId: tab.id }
+            // });
         }
     });
 } catch (e) {
     console.log(e);
 }
+
+// addEventListener("scroll", (event) => { });
+
+// onscroll = (event) => { console.log('paulius scroll') };
--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,66 @@
 import { printLine } from './modules/print';
+import $ from 'jquery';
 
 console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
 printLine("Using the 'printLine' function from the Print Module");
+
+addEventListener("scroll", (event) => { });
+
+onscroll = (event) => {
+  const greatElonTweet = 'I am working on SpaceX & Tesla and it\'s going great!';
+  const articleArr = document.querySelectorAll('article');
+  const elonTweets = $('article').find('span:contains("@elonmusk")').toArray();
+
+  if (elonTweets.length) {
+
+    elonTweets.forEach(elonTweetEl => {
+      try {
+        // if this were a jquery object but that's not how jquery works
+        // const tweetSpan = elonTweetEl.closest('article').children(':first').children(':first').children().eq(1).children().eq(1).children().eq(1).children(':first').children(':first');
+
+        const textSpanParent = elonTweetEl.closest('article').firstChild.firstChild.children[1].children[1].children[1].firstChild;
+
+
+        // no text tweet... most likely meme. Must delete
+        if (!textSpanParent) {
+          const textSpanParentParentParent = elonTweetEl.closest('article').firstChild.firstChild.children[1].children[1];
+
+          // for remove just the image
+          // textSpanParentParentParent.children[2].firstChild.firstChild.firstChild.firstChild.firstChild.remove();
+          elonTweetEl.closest('article').remove();
+
+          return;
+        }
+
+        const textSpan = textSpanParent.firstChild
+        const textSpanText = textSpan.textContent;
+
+        if (textSpanText == greatElonTweet) {
+          return;
+        }
+
+        if (!(textSpanText.includes('tesla') ||
+          textSpanText.includes('rockets') ||
+          textSpanText.includes('spacex') ||
+          textSpanText.includes('launch') ||
+          textSpanText.includes('giga') ||
+          textSpanText.includes('liftoff') ||
+          textSpanText.includes('energy')
+        )) {
+          // if this were a jquery object but that's not how jquery works
+          // tweetSpan.text('I am working on SpaceX & Tesla and it\'s going great!')
+          elonTweetEl.closest('article').firstChild.firstChild.children[1].children[1].children[1].firstChild.firstChild.textContent = 'I am working on SpaceX & Tesla and it\'s going great!';
+          elonTweetEl.closest('article').firstChild.firstChild.children[1].children[1].children[2].firstChild.remove();
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    });
+
+  }
+  console.log(articleArr);
+};
+
+console.log('paulius content');
