1925458b1759c5e800971ee61c15d1f95f2b9344
Paulius <pauliusdragunas@PAULIUSs-MacBook-Pro.local>
2023-03-07 10:55:47 -0800

    better retweet support, bump version to 0.0.3

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,24 +1,40 @@
 console.log('This is the background page.');
-console.log('Put the background scripts here.');
 
-console.log('paulius before try');
+let neededTabId;
 
 try {
-    console.log('paulius try');
     chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-        if (changeInfo.status == 'complete') {
-            console.log('paulius complete');
+        if (changeInfo.status == 'complete' && tab.url.includes('twitter')) {
+            neededTabId = tabId;
+            console.log('complete');
+
+            setListeners();
             // console.log('execute script about to run');
             // chrome.scripting.executeScript({
             //     files: ['contentScript.bundle.js'],
             //     target: { tabId: tab.id }
             // });
+
         }
     });
 } catch (e) {
     console.log(e);
 }
 
-// addEventListener("scroll", (event) => { });
-
-// onscroll = (event) => { console.log('paulius scroll') };
+function requestHandler(req) {
+    // tweets loaded, look for elon tweets
+    if (req.url.includes('UserTweets')) {
+        console.log('paulius req.url:', req.url);
+        // chrome.runtime.sendMessage('kckpbecdjciaioacmoeomgobhfomaolh', { action: "bringPeaceOfMind" });
+        chrome.tabs.sendMessage(neededTabId, {
+            action: "bringPeaceOfMind"
+        });
+    }
+}
+function setListeners() {
+    chrome.webRequest.onCompleted.addListener(requestHandler,
+        { urls: ["<all_urls>"] });
+}
+function removeListeners() {
+    chrome.webRequest.onCompleted.removeListener(requestHandler);
+}
--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,58 +1,81 @@
-import { printLine } from './modules/print';
 import $ from 'jquery';
+import { throttle as _throttle } from 'lodash';
 
-addEventListener("scroll", (event) => { });
+addEventListener("scroll", onscroll);
 
-onscroll = (event) => {
+onscroll = _throttle((event) => {
+  console.log('onscroll');
   bringPeaceOfMind();
-};
+}, 500);
 
-setTimeout(() => {
-  bringPeaceOfMind();
-}, 2000);
+chrome.runtime.onMessage.addListener(
+  function (request, sender, sendResponse) {
+    console.log(sender.tab ?
+      "from a content script:" + sender.tab.url :
+      "from the extension");
+    if (request.action === "bringPeaceOfMind") {
+      bringPeaceOfMind();
+      sendResponse({ farewell: "goodbye" });
+    }
+  }
+);
 
 function bringPeaceOfMind() {
   const greatElonTweet = 'I am working on SpaceX & Tesla and it\'s going great!';
+  const greatElonThirdPersonTweet = 'Elon is working on SpaceX & Tesla and it\'s going great!';
+
   const elonTweets = $('article').find('a[href$="/elonmusk"] div span:contains("@elonmusk")').toArray();
+  const elonRetweets = $('article').find('a[href$="/elonmusk"] span:contains("Retweeted")').toArray();
 
-  if (elonTweets.length) {
+  const doHtmlStuff = (elonTweetEl, replacementText) => {
+    try {
+      const textSpanParent = elonTweetEl.closest('article').firstChild.firstChild.children[1].children[1].children[1].firstChild;
 
-    elonTweets.forEach(elonTweetEl => {
-      try {
-        const textSpanParent = elonTweetEl.closest('article').firstChild.firstChild.children[1].children[1].children[1].firstChild;
+      // no text tweet... most likely meme. Must delete
+      if (!textSpanParent) {
+        const textSpanParentParentParent = elonTweetEl.closest('article').firstChild.firstChild.children[1].children[1];
 
+        elonTweetEl.closest('article').remove();
 
-        // no text tweet... most likely meme. Must delete
-        if (!textSpanParent) {
-          const textSpanParentParentParent = elonTweetEl.closest('article').firstChild.firstChild.children[1].children[1];
-
-          elonTweetEl.closest('article').remove();
-
-          return;
-        }
-
-        const textSpan = textSpanParent.firstChild
-        const textSpanText = textSpan.textContent;
-
-        if (textSpanText == greatElonTweet) {
-          return;
-        }
-
-        if (!(textSpanText.includes('tesla') ||
-          textSpanText.includes('rockets') ||
-          textSpanText.includes('spacex') ||
-          textSpanText.includes('launch') ||
-          textSpanText.includes('giga') ||
-          textSpanText.includes('liftoff') ||
-          textSpanText.includes('energy')
-        )) {
-          elonTweetEl.closest('article').firstChild.firstChild.children[1].children[1].children[1].firstChild.firstChild.textContent = 'I am working on SpaceX & Tesla and it\'s going great!';
-          elonTweetEl.closest('article').firstChild.firstChild.children[1].children[1].children[2].firstChild.remove();
-        }
-      } catch (error) {
-        console.log(error);
+        return;
       }
-    });
 
+      const textSpan = textSpanParent.firstChild
+      const textSpanText = textSpan.textContent;
+
+      if (textSpanText == greatElonTweet) {
+        return;
+      }
+
+      if (!(textSpanText.includes('tesla') ||
+        textSpanText.includes('rockets') ||
+        textSpanText.includes('spacex') ||
+        textSpanText.includes('launch') ||
+        textSpanText.includes('giga') ||
+        textSpanText.includes('liftoff') ||
+        textSpanText.includes('starship') ||
+        textSpanText.includes('dragon') ||
+        textSpanText.includes('energy')
+      )) {
+        // remove emojis at the beginning
+        while (elonTweetEl.closest('article').firstChild.firstChild.children[1].children[1].children[1].firstChild.firstChild.toString().includes('Image')) {
+          elonTweetEl.closest('article').firstChild.firstChild.children[1].children[1].children[1].firstChild.firstChild.remove();
+        }
+
+        // replace text
+        elonTweetEl.closest('article').firstChild.firstChild.children[1].children[1].children[1].firstChild.firstChild.textContent = replacementText;
+
+        // remove ending image
+        elonTweetEl.closest('article').firstChild.firstChild.children[1].children[1].children[2].firstChild.remove();
+
+        // remove ending link
+        elonTweetEl.closest('article').firstChild.firstChild.children[1].children[1].children[1].firstChild.children[1].remove();
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
+
+  elonTweets.forEach(elonTweetEl => doHtmlStuff(elonTweetEl, greatElonTweet));
+  elonRetweets.forEach(elonTweetEl => doHtmlStuff(elonTweetEl, greatElonThirdPersonTweet));
 }
